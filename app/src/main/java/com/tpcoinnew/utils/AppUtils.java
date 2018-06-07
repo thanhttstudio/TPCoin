3e56d9d8750f402e694b3c6c1afb408406c98776
thanh.tran <thanh.tran@apps-cyclone.com>
2018-06-07 08:15:00 +0700

    + remove branches dev2.

--- app/src/main/java/com/tpcoinnew/utils/AppUtils.java
+++ app/src/main/java/com/tpcoinnew/utils/AppUtils.java
@@ -1,12 +1,20 @@
 package com.tpcoinnew.utils;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.annotation.Nullable;
+import android.util.Base64;
+import android.util.Log;
 
 import com.tpcoinnew.interfaces.DialogListener;
 import com.tpcoinnew.others.dialog.AlertCustomDialog;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 /*
@@ -28,4 +36,19 @@ public class AppUtils {
         alertDialog.show();
     }
 
+    public static void getHashKey(Context context) {
+        try {
+            PackageInfo info = context.getPackageManager().getPackageInfo(
+                    "com.tpcoinnew",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException ignored) {
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
 }
